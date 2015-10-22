69a67506a47d6c5c1ff105e87e5a31dfa1a60056
unknown <jamouc@L4818-25.gigl.polymtl.ca>
2015-10-21 20:18:20 -0400

    Ajout Debut 6 Obnjet destructiblas

---
+++ Mecaniques/DestructibleObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestructibleObject : MonoBehaviour {
+
+	// Entre dans cette fonction lorsqu il y a une collision
+	void OnCollisionEnter(Collision collision) {
+	
+		// Si l objet rentre en contact avec un projectile
+		//if(collision.gameObject is Projectile){  
+			Destroy(collision.gameObject); // Destruction du projectile (A verifier avec ceux l implementation du projectile)
+			Destroy(this.gameObject); //Destruction de l obstacle ( Je pense a un bloc ou rocher )
+
+		//}
+
+		// Rajouter le cas pour les voitures pour qu il ralentisse a chaque degates
+
+	}
+
+
+
+}
